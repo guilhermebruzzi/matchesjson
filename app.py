f962bac68d90e0de300221d854b1359b1f3d8454
Guilherme Heynemann Bruzzi <guibruzzi@gmail.com>
2015-01-14 01:30:17 -0200

    Mudando a variavel de ambiente para pegar a conexao com o banco

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = os.environ.get('DEBUG', "True") == "True"
 local_connection = "mysql://root:@localhost/matches?charset=utf8&use_unicode=0"
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('CLEARDB_DATABASE_URL', local_connection)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI', local_connection)
 
 db = SQLAlchemy(app)
 
