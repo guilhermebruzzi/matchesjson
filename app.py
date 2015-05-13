687e6d87d5820340876d59021212ecde0eee2c21
Guilherme Heynemann Bruzzi <guilherme.bruzzi@corp.globo.com>
2015-05-13 14:44:22 -0300

    Fazendo o Flask servir o estatico e com header de expiracao

--- app.py
+++ app.py
@@ -5,34 +5,17 @@ from flask import Flask, Response
 from flask_cors import CORS
 from flask_cache_response_decorator import cache
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
 
 app.config['DEBUG'] = os.environ.get('DEBUG', "True") == "True"
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 60
 
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 @app.route("/")
 def index():
     return u"Use o /matches.json no exercicio."
-
-def read_file(filename, charset='utf-8'):
-    json_value = ""
-    with open(filename, 'r') as f:
-        json_value = f.read().decode(charset)
-    return json_value
-
-
-@app.route("/matches.json")
-@cache(expires=60)
-def list_matches():
-    json_value = read_file('matches.json')
-
-    rv = Response(
-        json_value,
-        content_type='application/json; charset=utf-8')
-
-    return rv
-
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
